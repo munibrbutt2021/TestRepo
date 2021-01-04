c19772058deabe23db32eb55ab5895544b3a18cb
Munib Butt <munibrbutt@rogers.com>
2021-01-04 14:48:32 -0500

    Changed message

--- ConsoleAppGitTest/Program.cs
+++ ConsoleAppGitTest/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppGitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from Munib!");
         }
     }
 }
